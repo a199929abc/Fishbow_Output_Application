2342f76b6d9d6890fe3240730c313a28cbc9f3d2
kaiheng <a199929abc123@gmail.com>
2021-04-23 12:47:11 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+## Overview
+FishPro (Fishbowl integrated App) is an application that reduces MTC staff repetitive work processes, it can generate the recovery template excel sheet base on the inventory number in the fishbowl system.
+
+### Input Section：
+As figure 1 shown, the only input the application needs is a **column of Inventory #** , be careful the header row format is fixed, DO NOT modify it. Once you fill in all the inventory # , save and close the document. you are ready to go.
+
+Figure 1. iupt template
+![alt text](/picture/input.JPG)
+
+### Login Section:
+After you click the .exe file. You will see a login page. Please use the username and password at the MTC fishbowl system. 
+
+Figure 2. login
+![alt text](/picture/login.JPG)
+
+### File read section:
+Once you login to the application, you will see a new window shown in figure 3, click **Explore** button to find the .xlsx file you prepared in the input section. After that, you will see the file path shown on the panel. Click **Enter** to run the application. If you accidentally choose the wrong file, you can simply Explore again, the new file path will be shown.
+
+
+Figure 3. Choose file
+
+![alt text](/picture/chooseFile.JPG)
+
+
+### Process section: 
+If you see a pop-up window shown in figure 4, that means you successfully create a recovery template. The output excel file will normally at the users' root directory. The output filename format: **out_YOUINPUTFILENAME.xlsx** if you can’t find it for some reason. Please search the format name on your computer. 
+
+Figure 4. process complete
+![alt text](/picture/processcomplete.JPG)
+
+### Output section: 
+After the process has completed, you will receive a new excel file as shown in figure 5. The program automatically fills the excel sheet based on the inventory provided. The Instrument name, serial number and device number getting from the fishbowl inventory system.
+
+Figure 5. output 
+
+![alt text](/picture/outputsample.JPG)
+
+## Description Area:
+Description area is the only place user need to pay extra attention. Because there are some custom fields that needs to be manually filled based on various situations and tasks. Such as **"METHOD","ATTENTION TO","RECEIVED", "NOTES", "LINK TO LOG"** etc. **"RECEIVED DATE"** automatically set to current date.
+
+Figure 6. description
+
+![alt text](/picture/description.JPG)
+
+# QA section:
+### 1. Why I can't login ? 
+There might be two possible error lead to this problem, 1) your username and password are wrong, please double-check and enter again. 2) Please check your Internet and retry.
+
+### 2. Get pop-up error message "Check row number 8 then retry"
+This happens because the description area in the inventory system doesn’t format right. There are some possible way to fix it.
+1) Open fishbowl API ->search the part number ->check the description area whether format as :”SN: XXX, DI:XXXXX” You can modify the description in the fishbowl system and retry.
+
+Figure 7. Error
+
+![alt text](/picture/error.JPG)
+
+### Format Issue
+Correct format :
+1. SN and DI exist. SN is in the front of DI :  "**EtherTek RMS-300 Monitor, SN: 00-50-C2-53-89-AC, DI: 24368**"
+2. SN and DI exist. DI is in the front of SN :  "**EtherTek RMS-300 Monitor, DI: 24368 SN: 00-50-C2-53-89-AC**"
+3. ONLY DI exist : "**EtherTek RMS-300 Monitor, DI: 24368**"
+4. ONLY SN exist : "**EtherTek RMS-300 Monitor, SN: 00-50-C2-53-89-AC**"
+5. DI and SN don't exist : "**EtherTek RMS-300 Monitor**"
+6. (Update) SN exist DI is pending : "**EtherTek RMS-300 Monitor, SN: 00-50-C2-53-89-AC DI: Pending**"  "pending" is Capital insensitive can be any format
+
+
+Error might show up to case below: 
+1. "EtherTek RMS-300 Monitor, SN 00-50-C2-53-89-AC, DI: 24368" Reason : "SN:" tag missing ":" can be add to fishbowl client
+2. "EtherTek RMS-300 Monitor, SN: 00-50-C2-53-89-AC, DI 24368" Reason : "DI:" tag missing ":" can be add to fishbowl client
+3. "EtherTek RMS-300 Monitor, SN: 00-50-C2-53-89-AC, DI: " Reason : After "DI: " tag missing value. If you don't know the DI, add "Pending" 
+4. "EtherTek RMS-300 Monitor, SN : 00-50-C2-53-89-AC, DI: Pending" Reason ： EXTRA SPACE in "SN :", should be "SN:" DI might have similar issuse
+5. More cases can be discovered in the Fishbowl client. If you are not sure where is the mistake, FOLLOW THE CORRECT format and modify the description
+
+
---
+++ src/main/java/App.java
@@ -0,0 +1,346 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.PasswordField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.formula.functions.T;
+import org.apache.poi.ss.usermodel.*;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.xml.sax.InputSource;
+import javafx.application.Application;
+import com.sun.scenario.effect.impl.prism.PrImage;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import  java.lang.Class.*;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class App extends Application {
+    public static String fileName;
+    public static String userName;// username
+    public static String passWord;// password
+    public static final String IA_NAME = "Java Sample"; //API name
+    public static final String IA_DESC = "Sample of Java Fishbowl connection";
+    public static final int IA_KEY = 54321;//Default key
+    public static String ticketKey;
+    public static String path;// file path
+    private static SAXBuilder builder = new SAXBuilder();
+    private static String hostName = "universityofvictoria.myfishbowl.com";
+    private static int port = 28192;
+
+
+    public static void main(String[] args) {
+        // Create new object of this class
+
+        launch();//UI
+        Connection connection = new Connection(hostName, port);
+        // Create login XML request and send to the server
+
+        String loginRequest = Requests.loginRequest(App.userName,App.passWord);
+        String response = connection.sendRequest(loginRequest);
+        if (response == null) {
+            connection.disconnect();
+            System.out.println("response is Null");
+            System.exit(1);
+        }
+        // Parse response
+        Document document = null;
+        try {
+            document = parseXml(response);
+        } catch (Exception e) {
+            System.out.println("Cannot parse the xml response.");
+            connection.disconnect();
+            System.exit(1);
+        }
+        Element root = document.getRootElement();
+        String statusCode = root.getChild("FbiMsgsRs").getAttribute("statusCode").getValue();
+
+        if (!statusCode.equals("1000")) {
+            System.out.println("The integrated application needs to be approved.");
+            connection.disconnect();
+            System.exit(0);
+        }
+        // Login successful, store the key
+        ticketKey = root.getChild("Ticket").getChild("Key").getValue();
+        Element node = null;
+
+
+        write_to_Excel(connection,response);//run write excel code
+        App.infoBox("Process Completed ! ","successful");
+        System.exit(1);
+
+    }
+    /**
+     * @param xml
+     * @return
+     * @throws IOException
+     * @throws JDOMException
+     */
+    private static Document parseXml(String xml) throws IOException, JDOMException {
+        return builder.build(new InputSource(new StringReader(xml)));
+    }
+    /**
+     *
+     * @param response
+     * @return String description
+     */
+    private static String getDescription(String response){
+        String result = response.substring(response.indexOf("<Description>")+13,response.indexOf("</Description>"));
+        return result;
+    }
+    /**
+     * @param primaryStage
+     * @throws Exception
+     */
+    public void start(Stage primaryStage) throws Exception {
+     primaryStage.setTitle("Login");//https://blog.csdn.net/guanguoxiang/article/details/45461879
+     GridPane grid =new GridPane();
+     grid.setAlignment(Pos.CENTER);
+     grid.setHgap(10);
+     grid.setVgap(10);
+     grid.setPadding(new Insets(25,25,25,25));
+     Scene sence =new Scene(grid,350,275);
+
+
+    Text scenetitle = new Text("Login Fishbowl ");
+    scenetitle.setFont(Font.font(20));
+    grid.add(scenetitle, 0, 0, 2, 1);
+
+
+
+    Label username = new Label("Username:");
+    grid.add(username, 0, 1);
+    username.setFont(Font.font(13));
+
+
+    TextField userTextField = new TextField();
+    grid.add(userTextField, 1, 1);
+
+    Label pw = new Label("Password:");
+    pw.setFont(Font.font(13));
+    grid.add(pw, 0, 2);
+
+    PasswordField pwBox = new PasswordField();
+    grid.add(pwBox, 1, 2);
+
+    Button btn_login = new Button("Sign in");
+    HBox hbBtn = new HBox(10);
+
+    hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+    hbBtn.getChildren().add(btn_login);
+    grid.add(hbBtn, 1, 4);
+    Text actiontarget = new Text();
+    actiontarget.setFont(Font.font(13));
+    grid.add(actiontarget, 1, 6);
+
+    btn_login.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+            actiontarget.setFill(Color.RED);
+            actiontarget.setText("Connecting to server . . . . . . ");
+            userName=userTextField.getText();
+            passWord=pwBox.getText();
+            Connection connection = new Connection(hostName, port);
+            // Create login XML request and send to the server
+            String loginRequest = Requests.loginRequest(userName,passWord);
+            String response = connection.sendRequest(loginRequest);
+            // Parse response
+            Document document = null;
+            try {
+                document = parseXml(response);
+            } catch (Exception e) {
+                System.out.println("Cannot parse the xml response.");
+                connection.disconnect();
+                System.exit(1);
+            }
+            Element root = document.getRootElement();
+            String statusCode = root.getChild("FbiMsgsRs").getAttribute("statusCode").getValue();
+
+            if (statusCode.equals("1000")) {
+                //connection build
+                connection.disconnect();
+                primaryStage.close();
+                Stage secondStage = new Stage();
+                gotoFile(secondStage);
+            }else{
+               App.infoBox("username and password doesn't match with system ", "Login Error");
+            }
+
+        }
+    });
+    primaryStage.setScene(sence);
+    primaryStage.show();
+    }
+    /**
+     *
+     * @param secondStage
+     */
+    private void gotoFile(Stage secondStage) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        System.out.println(System.getProperty("user.home"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Excel", "*.xlsx")
+        );
+        Text fileChoose = new Text("Please Choose a File ");
+        fileChoose.setFont(Font.font(15));
+
+        Button btnProcess =new Button ("Enter");
+        Button btnFile = new Button("Explore");
+        btnProcess.setMaxSize(150,150);
+        btnFile.setMaxSize(150,150);
+        GridPane gridPane = new GridPane();
+        gridPane.setMinSize(400, 200);
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        //Setting the vertical and horizontal gaps between the columns
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+        gridPane.add(fileChoose,0,0);
+        gridPane.add(btnFile,0,1);
+        gridPane.add(btnProcess,0,2);
+
+        //Setting the Grid alignment
+        gridPane.setAlignment(Pos.TOP_LEFT);
+        Scene sceneFile = new Scene(gridPane);
+        secondStage.setTitle("Open File ");
+        btnFile.setOnAction(new EventHandler<ActionEvent>() {
+            int i=2;
+            public void handle(ActionEvent actionEvent) {
+                i+=1;
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))
+                );
+                fileChooser.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("Excel", "*.xlsx")
+
+                );
+                File file =fileChooser.showOpenDialog(null);
+                path = file.getPath();
+                Label labelPath1 = new Label(path);
+                labelPath1.setTextFill(Color.BLUE);
+                System.out.println("path is "+path);
+                gridPane.add(labelPath1,0,i);
+
+            }
+        });
+        btnProcess.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                fileName = path.substring(path.lastIndexOf("\\")+1);
+                infoBox(("Processing file "+fileName),"Successful");
+                secondStage.close();
+            }
+        });
+        secondStage.setScene(sceneFile);
+        secondStage.show();
+    }
+
+    public static void infoBox(String infoMessage, String titleBar) {
+        JOptionPane.showMessageDialog(null, infoMessage, titleBar, JOptionPane.INFORMATION_MESSAGE);
+    }
+    /**
+     *
+     * @param connection
+     * @param response
+     */
+    public static void write_to_Excel(Connection connection,String response){
+        try {
+            //创建工作簿
+            FileInputStream is = new FileInputStream(App.path);
+            excelReader reader = new excelReader(is);
+            reader.read(is);
+
+            int max_row = reader.maxRow();
+
+            // int title =reader.getMapTitle("Serial #");get mapping title
+            excelWriter ew = new excelWriter(path, "name");
+            //Writing Header to the sheet
+            ew.write_header();
+            int count=0;
+      try{
+          ArrayList collect=new ArrayList();
+          for (int i = 1; i < max_row; i++) {
+              System.out.println(i);
+              count=i+1;
+              collect= reader.processRow(i);
+              if (collect.get(0) == null && collect.get(1) == null && collect.get(2) == null) {
+                  System.out.println("Process have completed！");
+                  break;
+              }
+          if (collect.get(2) != null) {
+            BigInteger partid = new BigDecimal(collect.get(2).toString()).toBigInteger();
+            // get Part XML format
+            String getPart = Requests.get_part(partid);
+            // connect to API and get response back
+            response = connection.sendRequest(getPart);
+            // get Description String
+            String result = getDescription(response).replaceAll(",","");
+
+            if(result.indexOf("DI:")!=-1){
+
+                String temp = result.substring(result.indexOf("DI:") + 4);
+                //System.out.println(temp);
+                ArrayList<String> rowFill = new ArrayList<String>();
+
+                rowFill.add(temp.substring(0, temp.indexOf("")));
+                rowFill.add(temp);
+                System.out.println(temp);
+                System.out.println("??????????????"+temp.substring(0, temp.indexOf("")));
+                ew.write_row(rowFill, i);
+            }
+
+            }
+
+
+
+                }
+              //end of for
+      } catch (Exception e){
+          App.infoBox(String.format("ERROR: Please check row number %s then retry",count),"Error");
+          e.printStackTrace();
+          System.exit(1);
+      }
+            String outpath=System.getProperty("user.home");
+            String output_path =String.format("%s\\out_%s",outpath,fileName);
+            System.out.println(output_path);
+
+            ew.create_excel(output_path);//Write to excel
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
